27393e01d8338b5d8077b2146d49b321481f942b
Alex Britez <abritez@gmail.com>
2017-10-05 16:57:37 -0400

    Initial commit for blogs

---
+++ blog/blogData.js
@@ -0,0 +1,50 @@
+var blog = {
+	title: "My Blog",
+	posts: [
+
+		{
+			title: "Title A",
+			body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo quia tempora porro, sed aliquam voluptates sit libero quod ipsum repellat laudantium explicabo quisquam odio, vel dolor laborum unde commodi officiis!",
+			comments: [
+				{
+					profile_img: "https://placeimg.com/50/50/people?i=1",
+					comment: "This is a comment A"
+				},
+				{
+					profile_img: "https://placeimg.com/50/50/people?i=2",
+					comment: "This is a comment B"
+				}
+			]
+		
+		},
+
+		{
+			title: "Title B",
+			body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo quia tempora porro, sed aliquam voluptates sit libero quod ipsum repellat laudantium explicabo quisquam odio, vel dolor laborum unde commodi officiis!",
+			comments: [
+				{
+					profile_img: "https://placeimg.com/50/50/people?i=3",
+					comment: "This is a comment C"
+				}
+			]
+		
+		},
+
+		{
+			title: "Title C",
+			body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo quia tempora porro, sed aliquam voluptates sit libero quod ipsum repellat laudantium explicabo quisquam odio, vel dolor laborum unde commodi officiis!",
+			comments: [
+				{
+					profile_img: "https://placeimg.com/50/50/people?i=4",
+					comment: "This is a comment D"
+				},
+				{
+					profile_img: "https://placeimg.com/50/50/people?i=5",
+					comment: "This is a comment E"
+				}
+			]
+		
+		}
+
+	]
+}
