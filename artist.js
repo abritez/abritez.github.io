b9f9ee0a1f5bed93ec142d3404f0af71924552af
Alex Britez <abritez@gmail.com>
2017-10-05 14:52:22 -0400

    add artist updates

---
+++ artist.js
@@ -0,0 +1,10 @@
+//Javascript Object of a user
+var users = [{
+  firstname : "Leonardo",
+  lastname: "da Vinci",
+  hobbies: ["paint", "sculpt", "invent", "do math"]
+},{
+  firstname : "Pablo",
+  lastname: "Picaso",
+  hobbies: ["draw",  "paint"]
+}];
