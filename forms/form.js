cb76f394c7b073edd134d4290f1162a135fdc531
Alex Britez <abritez@gmail.com>
2017-10-06 12:40:04 -0400

    added challenge requirements and comments

--- forms/form.js
+++ forms/form.js
@@ -7,13 +7,22 @@ function handleSubmission(event){
 	
 	console.log(registraion_form.elements);
 
-	
 	var username = registraion_form.elements.username;
 	var password = registraion_form.elements.password;
 	var confirm = registraion_form.elements.confirm;
 	var hobbies = registraion_form.elements.hobbies;
 
+	/*push any hobbies that were checked into an array*/
+	var hobbiesArray = [];
+	for(var i=0; i< hobbies.length; i++){
+		if(hobbies[i].checked){
+			hobbiesArray.push(hobbies[i].value)
+		}
+	}
+
 	console.log(username.value.length);
+
+	/*Validation Code*/
 
 	if(username.value.length < 3){
 		alert("Please make sure you use at least 5 charectors!");
@@ -25,19 +34,8 @@ function handleSubmission(event){
 		return false;
 	}
 
-	registraion_form.elements.hobbies.forEach(function(e, x){
-		console.log(e.checked + " " + x)
-	})
 
 
-	var hobbiesArray = [];
-
-	for(var i=0; i< hobbies.length; i++){
-		if(hobbies[i].checked){
-			hobbiesArray.push(hobbies[i].value)
-		}
-		
-	}
 	
 }
 
