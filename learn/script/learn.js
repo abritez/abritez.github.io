98b36f7c0b9110f08626d9e0b231e4ca1ae4ff3b
Alex Britez <abritez@gmail.com>
2017-10-15 09:30:42 -0400

    init Box Model commit

---
+++ learn/script/learn.js
@@ -0,0 +1,9 @@
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/twilight");
+editor.session.setMode("ace/mode/css");
+//editor.setReadOnly(true);
+
+// var csseditor = ace.edit("csseditor");
+// csseditor.setTheme("ace/theme/twilight");
+// csseditor.session.setMode("ace/mode/css");
+// csseditor.setReadOnly(true);
