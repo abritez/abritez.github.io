d7a562a048804fade486d1d3fdd3a977d785d510
Alex Britez <abritez@gmail.com>
2017-10-16 12:43:17 -0400

    Adding Display page

--- learn/script/learn.js
+++ learn/script/learn.js
@@ -1,9 +1,20 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/twilight");
-editor.session.setMode("ace/mode/css");
+editor.session.setMode("ace/mode/html");
+editor.setReadOnly(true);
+
+var editorB = ace.edit("editorB");
+editorB.setTheme("ace/theme/twilight");
+editorB.session.setMode("ace/mode/css");
 //editor.setReadOnly(true);
 
 // var csseditor = ace.edit("csseditor");
 // csseditor.setTheme("ace/theme/twilight");
 // csseditor.session.setMode("ace/mode/css");
 // csseditor.setReadOnly(true);
+
+
+
+editorB.on('change', function(e){
+	console.log("CHange");	
+});
