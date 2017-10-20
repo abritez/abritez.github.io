90ff36980a36d7edc465d30b1cf904205a7d85db
Alex Britez <abritez@gmail.com>
2017-10-20 16:34:34 -0400

    Helper

---
+++ learn/classfiles/items.js
@@ -0,0 +1,44 @@
+var items = [
+	
+	{
+		image: "http://lorempixel.com/300/200/nature/",
+		caption: "Some Caption 1"
+	},
+	{
+		image: "http://lorempixel.com/300/201/nature/",
+		caption: "Some Caption 2"
+	},
+	{
+		image: "http://lorempixel.com/300/202/nature/",
+		caption: "Some Caption 3"
+	},
+	{
+		image: "http://lorempixel.com/300/203/nature/",
+		caption: "Some Caption 4"
+	},
+	{
+		image: "http://lorempixel.com/300/204/nature/",
+		caption: "Some Caption 5"
+	},
+	{
+		image: "http://lorempixel.com/300/205/nature/",
+		caption: "Some Caption 6"
+	},
+	{
+		image: "http://lorempixel.com/300/206/nature/",
+		caption: "Some Caption 7"
+	},
+	{
+		image: "http://lorempixel.com/300/207/nature/",
+		caption: "Some Caption 8"
+	},
+	{
+		image: "http://lorempixel.com/300/208/nature/",
+		caption: "Some Caption 9"
+	},
+	{
+		image: "http://lorempixel.com/300/209/nature/",
+		caption: "Some Caption 10"
+	}
+
+]
